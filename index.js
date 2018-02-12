55eda83354355dfb92442560a796dcc1c3fbd003
Louis Shi <louis.shi@uconn.edu>
2018-02-12 04:07:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function getFirstSelector(selector) {
 function nestedTarget(){
   return document.getElementById("nested").querySelector(".target");
 }
+function deepestChild() {
+  var lis = document.getElementById("grand node").querySelectorAll("div");
+  var test;
+  for (let i = 0; i < lis.length-1; i++) {
+    test = lis[i].querySelector("div");
+  }
+  return test;
+}
